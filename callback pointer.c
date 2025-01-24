7f5c9604b6aedd8b1c9daa5e108a09f2dcf59e6a
parag-vora <voraparag10@gmail,com>
2025-01-24 13:34:53 +0530

    first commit

--- callback pointer.c
+++ callback pointer.c
@@ -2,23 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void fun(){
+void fun(){     // function return type void
     printf("hello world\n");
 }
 
-int *ptr_fun(){
+int *ptr_fun(){   // function return integer pointer
 
     static int arr[] = {1,2,3};
 
     return arr;
 }
 
-void callback(void (*ptr)()){
+void callback(void (*ptr)()){  // normal callback
 
     ptr();
 }
 
-void ptr_callbakc(int (*(*ptr1)())){
+void ptr_callbakc(int (*(*ptr1)())){  // callback function accept pointer of interger pointer function
 
     int *arr1 = ptr1();
 
@@ -28,13 +28,13 @@ void ptr_callbakc(int (*(*ptr1)())){
 
 int main() {
 
-void (*ptr)() = &fun;
+void (*ptr)() = &fun;    // function pointer
 
-int (*(*ptr1)()) = &ptr_fun;
+int (*(*ptr1)()) = &ptr_fun;  // function pointer of function which return integer pointer
 
-callback(ptr);
+callback(ptr);   // call the callback function
 
-ptr_callbakc(ptr1);
+ptr_callbakc(ptr1);    // call the callback function
 
     return 0;
 }

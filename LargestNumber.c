c81e99b57b11268a5c7a17fa4994c9137ab2e72a
parag-vora <voraparag10@gmail,com>
2025-07-12 11:57:50 +0200

    find a largest number from array. dynamic memory allocation

--- LargestNumber.c
+++ LargestNumber.c
@@ -2,14 +2,26 @@
 // This program finds the largest number in an array of integers.
 
 #include <stdio.h>
-#define Size 5      // Define the size of the array by a macro
+#include <stdlib.h> 
 
-int main(){
-    int arr[Size];
-    printf("Enter numbers in the array:\n");
-    // Using scanf to read integers into the array
-    for(int i = 0; i < Size; i++){
-        scanf("%d", &arr[i]);
+int main()
+{
+    int Size;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &Size);
+
+    int *arr = (int *)malloc(Size * sizeof(int));
+    if (arr == NULL)
+    {
+        printf("Dynamic Memory allocation failed\n");
+        exit(0); // Exit if memory allocation fails
+    }
+
+    printf("Enter numbers in the array: \n");
+    for (int i = 0; i < Size; i++)
+    {
+        scanf("%d", (arr + i));  // Using pointer arithmetic to write values
     }
 
     int largest = arr[0];
@@ -18,8 +30,11 @@ int main(){
         if (arr[i] > largest)
         {
             largest = arr[i];
-        }       
+        }
     }
     printf("\nLargest element in the array is: %d\n", largest);
-    return 0;    
+
+    free(arr);  // Free the allocated memory
+    arr = NULL; // to avoid dangling pointer
+    return 0;
 }

5445e08abbec355832ad8f854e1235fbe40acf6e
parag-vora <voraparag10@gmail.com>
2025-07-25 21:40:12 +0200

    Refactor returnAddress.c to include necessary headers and clarify memory management comments

--- returnAddress.c
+++ returnAddress.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h> // For malloc and free
 
 int *getFun(); // Function prototype // forward declaration
 
@@ -7,11 +8,18 @@ int main(){
     int *ptr = getFun();
     printf("arr[o] = %d, arr[1] = %d\n", ptr[0], ptr[1]);
 
+    free(ptr); // Freeing the allocated memory is not necessary here, as we are returning the address
+    
     return 0;
 }
 
 int *getFun(){
 
-    static int arr[2] = {25, 0}; // Using static to return address of local variable
+    //static int arr[2] = {25, 0}; // Using static to return address of local variable^
+
+    int *arr = (int *)malloc(2 * sizeof(int)); // Dynamically allocating memory for the array
+    arr[0] = 25; // Assigning values to the array
+    arr[1] = 0; // Assigning values to the array
+
     return arr; // Returning address of the array
 }

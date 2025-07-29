0854f374c25b076a8beb43a93e1e1d6152b4d75a
parag-vora <voraparag10@gmail.com>
2025-07-29 20:33:44 +0200

    add a typedef for function pointer and switch case for choice(1. square and 2. cube)

--- callback/arraySquareandCube.c
+++ callback/arraySquareandCube.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdint.h>
 
+typedef void (*FPTR)(int *, int);
+
 void square(int *arr, int size){
     printf("___Square___\n");
     for(uint8_t i = 0; i < size; i++)
@@ -21,10 +23,26 @@ void fun(void (*fptr)(int *, int), int *arr, int size){
 }
 
 int main(){
-    void (*ptr)(int *, int);
+    //void (*ptr)(int *, int);
+    uint8_t choice;
+    FPTR ptr = NULL;
+    printf("Enter a choice 1. Square and 2. Cube: ");
+    scanf("%u", &choice);
     int arr[] = {1,2,3};
     uint8_t size = sizeof(arr)/sizeof(arr[0]);
-    ptr = cube;
+
+    switch (choice)
+    {
+    case 1:
+        ptr = square;
+        break;
+    case 2:
+        ptr = cube;
+        break;
+    default:
+        break;
+    }
+
     fun(ptr, arr, size);
     
     return 0;

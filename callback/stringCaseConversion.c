21cb4a931762c84e552f0a58eeefd854db051c89
parag-vora <voraparag10@gmail.com>
2025-07-29 21:23:09 +0200

    add a typedef for primitive data and function pointer, add a switch case for choice, add a string input from user and add a user choice for selection.

--- callback/stringCaseConversion.c
+++ callback/stringCaseConversion.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 #include<string.h>
 
-void Uppercase(char *str){
+typedef void(*FPTR)(char *);
+typedef char* CPTR;
+typedef int INTEGER;
+typedef char CHARACTER;
+
+void Uppercase(CPTR str){
     while(*str != '\0'){
         if(*str >= 65 &&  *str <= 90){
             printf("%c", *str);
@@ -12,7 +17,7 @@ void Uppercase(char *str){
     }
 }
 
-void Lowercase(char *str){
+void Lowercase(CPTR str){
         while(*str != '\0'){
         if(*str >= 97 &&  *str <= 122){
             printf("%c", *str);
@@ -23,13 +28,30 @@ void Lowercase(char *str){
     }
 }
 
-void fun(void (*fptr)(char *), char *str){
+void fun(FPTR fptr, CPTR str){
         (fptr)(str);
 }
 
 int main(){
-    void (*fptr)(char *) = Uppercase;
-    char *ptr = "hello";
+    INTEGER choice;
+    CHARACTER ptr[20];
+    FPTR fptr = NULL;
+    printf("Enter a string: ");
+    scanf("%s", ptr);
+    printf("Enter your choice 1. Lowercase 2. Uppercase : ");
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+    case 1:
+        fptr = Lowercase;
+        break;
+    case 2:
+        fptr = Uppercase;
+    default:
+        break;
+    }
+
     fun(fptr, ptr);
     return 0;
 }
